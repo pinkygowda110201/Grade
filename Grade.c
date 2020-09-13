bce053fb01b35553624f96159768e695a93cd1b7
pinkygowda110201 <71083770+pinkygowda110201@users.noreply.github.com>
2020-09-13 21:46:00 +0530

    Add files via upload

---
+++ Grade.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+main ()
+{
+	int marks;
+	printf("Enter your marks: ");
+	scanf("%d",&marks);
+	if(marks>=85)
+	{
+		printf("\nGrade A");
+	}
+	else if(marks>=70)
+	{
+		printf("\nGrade B");
+	}
+	else if (marks>=55)
+	{
+		printf("\nGrade C");
+	}
+	else if(marks>=40)
+	{
+		printf("\nGrade D");
+	}
+	else
+	{
+		printf("\nGrade F");
+	}
+}
